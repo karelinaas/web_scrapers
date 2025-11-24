bb3838332437aa0d09df0a75cf8a024cb788ec57
aleksandraminina <a.minina@netrika.ru>
2025-11-24 15:16:57 +0300

    Console arguments support for main script

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import asyncio
+import sys
 
 from demo_utils import demo_async_scraper, demo_sync_scraper
 from scrapers import AiohttpBasedScraper, HttpxBasedScraper, MultiThreadRequestsBasedScraper
 
-
-async def main():
-    await demo_async_scraper(AiohttpBasedScraper)
-    await demo_async_scraper(HttpxBasedScraper)
-    demo_sync_scraper()
-    demo_sync_scraper(MultiThreadRequestsBasedScraper, many_queries=True)
+async def main(sys_argv: list[str]):
+    if any(code in sys_argv for code in ["aiohttp", "all"]):
+        await demo_async_scraper(AiohttpBasedScraper)
+    if any(code in sys_argv for code in ["httpx", "all"]):
+        await demo_async_scraper(HttpxBasedScraper)
+    if any(code in sys_argv for code in ["requests", "all"]):
+        demo_sync_scraper()
+    if any(code in sys_argv for code in ["requests_multi", "all"]):
+        demo_sync_scraper(MultiThreadRequestsBasedScraper, many_queries=True)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    sys_argv = sys.argv
+    asyncio.run(main(sys.argv or ["all"]))
